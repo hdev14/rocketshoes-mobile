4902821c71b1ab3f6184f06ba461b74c9a1b929f
HermersonDev <ne.hermerson@gmail.com>
2020-03-17 10:10:10 -0300

    :construction: adds reactotron enchancer into redux store

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {Provider} from 'react-redux';
 
-import store from './store';
 import './configs/ReactotronConfig';
+import store from './store';
 
 import Routes from './routes';
 
