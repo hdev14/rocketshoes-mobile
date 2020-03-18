8205f7d6b834afe2735b38a310829f982b5fd2bf
HermersonDev <ne.hermerson@gmail.com>
2020-03-18 15:53:25 -0300

    :recycle: refactoring cart reducer

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -4,25 +4,10 @@ export default function cart(state = [], action) {
   switch (action.type) {
     case '@cart/ADD_TO_CART_SUCCESS':
       return produce(state, draftState => {
-        const productIndex = draftState.findIndex(
-          product => product.id === action.product.id,
-        );
-
-        if (productIndex >= 0) {
-          draftState[productIndex].amount += 1;
-        } else {
-          draftState.push({
-            ...action.product,
-            amount: 1,
-          });
-        }
+        draftState.push(action.product);
       });
 
-    case '@cart/UPDATE_PRODUCT_AMOUNT_SUCCESS': {
-      if (action.amount <= 0) {
-        return state;
-      }
-
+    case '@cart/UPDATE_PRODUCT_AMOUNT_SUCCESS':
       return produce(state, draftState => {
         const productIndex = draftState.findIndex(
           product => product.id === action.id,
@@ -32,7 +17,6 @@ export default function cart(state = [], action) {
           draftState[productIndex].amount = action.amount;
         }
       });
-    }
 
     case '@cart/REMOVE_FROM_CART':
       return produce(state, draftState => {
