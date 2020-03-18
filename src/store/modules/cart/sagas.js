2ff4ec7c453b24a739f962750a16c2057442fcfe
HermersonDev <ne.hermerson@gmail.com>
2020-03-18 15:57:13 -0300

    :construction: adds alert for product amount in stock

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,11 +1,49 @@
-import {call, put, all, takeLatest} from 'redux-saga/effects';
+import {Alert} from 'react-native';
+import {call, put, all, select, takeLatest} from 'redux-saga/effects';
 
-import {addToCartSuccess} from './actions';
+import {
+  addToCartSuccess,
+  updateAmountSuccess,
+  updateAmountRequest,
+} from './actions';
 import api from '../../../services/api';
 
 function* addToCart({id}) {
-  const response = yield call(api.get, `/products/${id}`);
-  yield put(addToCartSuccess(response.data));
+  const productExists = yield select(state =>
+    state.cart.find(p => p.id === id),
+  );
+
+  if (!productExists) {
+    const response = yield call(api.get, `/products/${id}`);
+    yield put(
+      addToCartSuccess({
+        ...response.data,
+        amount: 1,
+      }),
+    );
+    return;
+  }
+
+  yield put(updateAmountRequest(productExists.id, productExists.amount + 1));
 }
 
-export default all([takeLatest('@cart/ADD_TO_CART_REQUEST', addToCart)]);
+function* updateAmount({id, amount}) {
+  if (amount <= 0) {
+    return;
+  }
+
+  const stock = yield call(api.get, `/stock/${id}`);
+  const amountInStock = stock.data.amount;
+
+  if (!(amount > amountInStock)) {
+    yield put(updateAmountSuccess(id, amount));
+    return;
+  }
+
+  Alert.alert('Quantidade indisponível no estoque.');
+}
+
+export default all([
+  takeLatest('@cart/ADD_TO_CART_REQUEST', addToCart),
+  takeLatest('@cart/UPDATE_PRODUCT_AMOUNT_REQUEST', updateAmount),
+]);
